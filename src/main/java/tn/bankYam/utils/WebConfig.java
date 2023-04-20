9db353d00c3a0cbbf44819a9de2da192ce20fe33
cycy6031 <dqas1004t@naver.com>
2023-04-20 15:37:34 +0900

    채팅방

--- src/main/java/tn/bankYam/utils/WebConfig.java
+++ src/main/java/tn/bankYam/utils/WebConfig.java
@@ -1,10 +1,14 @@
 package tn.bankYam.utils;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 	String urls[] = {
 
@@ -22,4 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
 				.addPathPatterns("/**")
 				.excludePathPatterns(urls);
 	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+
 }
