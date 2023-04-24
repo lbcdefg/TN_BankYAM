6841c0ccef4346f57d784fd7ca56449c19732b82
zenxxn <bunbun2438@naver.com>
2023-04-24 12:21:10 +0900

    계좌이체월욜

--- src/main/java/tn/bankYam/service/AccountyServiceImpl.java
+++ src/main/java/tn/bankYam/service/AccountyServiceImpl.java
@@ -14,59 +14,20 @@ public class AccountyServiceImpl implements AccountyService{
     @Autowired
     AccountyMapper mapper;
 
+
     @Override
-    public boolean checkAccnumS(long ac_seq) {
-        if(mapper.checkAccnum(ac_seq)!=null){
-            System.out.println("");
-            return true;
-        }else{
-            System.out.println("");
-            return false;
-        }
+    public List<Accounty> selectAccNumS(long ac_seq) {
+        return mapper.selectAccNum(ac_seq);
     }
 
     @Override
-    public boolean checkPwdS(String ac_pwd) {
-        if(mapper.checkPwd(ac_pwd) !=null){
-            System.out.println("");
-            return true;
-        }else {
-            System.out.println("");
-            return false;
-        }
+    public Accounty selectAccInfoS(String ac_pwd) {
+        return mapper.selectAccInfo(ac_pwd);
     }
 
     @Override
-    public boolean checkStatusS(String ac_status) {
-        if(mapper.checkStatus(ac_status)=="사용중"){
-            return true;
-        }else if(mapper.checkStatus(ac_status)=="해지"){
-            System.out.println("");
-            return false;
-        }else {
-            System.out.println("");
-            return false;
-        }
-    }
-
-    @Override
-    public boolean checkPdS(long ac_pd_seq) {
-        if(mapper.checkPd(ac_pd_seq)==1){
-            return true;
-        }else{
-            System.out.println("");
-            return false; 
-        }
-    }
-
-    @Override
-    @Transactional //(rollbackOn = RollBackException.class, dontRollbackOn = RuntimeException.class)
-    public boolean transferS(Accounty accounty) {
-        if(checkAccnumS(accounty.getAc_seq()) && checkPwdS(accounty.getAc_pwd()) && checkStatusS(accounty.getAc_status())&&checkPdS(accounty.getAc_pd_seq()))
-            return mapper.transfer(accounty);
-        else {
-            return false;
-        }
+    public void transferS(Accounty accounty) {
+        mapper.transfer(accounty);
     }
 
     @Override
@@ -76,6 +37,7 @@ public class AccountyServiceImpl implements AccountyService{
 
     @Override
     public Product findPdBySeq(long seq){
+
         return mapper.findPdBySeq(seq);
     }
     @Override
